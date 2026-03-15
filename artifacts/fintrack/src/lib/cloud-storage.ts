3df759d6d03a860822b556af711545539b60c26f
maxmitsu <george.fuertes90@gmail.com>
2026-03-15 14:41:19 -0400

    Create cloud-storage.ts

---
+++ artifacts/fintrack/src/lib/cloud-storage.ts
@@ -0,0 +1,44 @@
+import {
+  ensureAppFile,
+  initGoogleAuth,
+  loadDriveData,
+  loginWithGoogleDrive,
+  saveDriveData,
+} from "./google-drive";
+
+const FILE_ID_KEY = "fintrack_drive_file_id";
+const STATE_KEY = "fintrack_state";
+
+export async function connectGoogleDrive() {
+  initGoogleAuth();
+  await loginWithGoogleDrive();
+  const fileId = await ensureAppFile();
+  localStorage.setItem(FILE_ID_KEY, fileId);
+  return fileId;
+}
+
+export function getDriveFileId() {
+  return localStorage.getItem(FILE_ID_KEY);
+}
+
+export function getLocalState() {
+  return JSON.parse(localStorage.getItem(STATE_KEY) || "{}");
+}
+
+export function setLocalState(data: unknown) {
+  localStorage.setItem(STATE_KEY, JSON.stringify(data));
+}
+
+export async function pullCloudState() {
+  const fileId = getDriveFileId();
+  if (!fileId) throw new Error("No hay archivo vinculado");
+  const data = await loadDriveData(fileId);
+  setLocalState(data);
+  return data;
+}
+
+export async function pushCloudState(data?: unknown) {
+  const fileId = getDriveFileId();
+  if (!fileId) throw new Error("No hay archivo vinculado");
+  await saveDriveData(fileId, data ?? getLocalState());
+}
