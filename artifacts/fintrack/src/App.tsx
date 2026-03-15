e56a0bd1e54a193baee1699124849cf614cec100
maxmitsu <george.fuertes90@gmail.com>
2026-03-15 13:00:57 -0400

    Update App.tsx

--- artifacts/fintrack/src/App.tsx
+++ artifacts/fintrack/src/App.tsx
@@ -1,5 +1,5 @@
 import { Switch, Route, Router as WouterRouter } from "wouter";
-import useHashLocation from "wouter/use-hash-location";
+import { useHashLocation } from "wouter/use-hash-location";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
